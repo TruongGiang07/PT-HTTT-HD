5d33575c231a12a273a0fd28f2ddc36bb261e5d8
huynhchinhhao <phantom_knight_vn@yahoo.com>
2017-04-16 17:12:47 +0700

    add SoTietKiemController.cs add SoTietKiemBUS.cs

---
+++ Code/CSharp.MiddleWare/BankManagement-MiddleWare/BankMgmt.MW.API/Controllers/SoTietKiemController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BankMgmt.MW.DataAccessLayer;
+
+namespace BankMgmt.MW.API.Controllers
+{
+    public class SoTietKiemController : ApiController
+    {
+        [HttpGet]
+        [Route("api/stk/all")]
+        public IHttpActionResult GetAll()
+        {
+            var stkBus = new BankMgmt.MW.BusinessLayer.SoTietKiemBUS();
+            return Ok(stkBus.GetAllSoTietKiem());
+        }
+
+        [HttpPost]
+        [Route("api/stk/add")]
+        public HttpResponseMessage Add([FromBody]SoTietKiem stk)
+        {
+            var stkBus = new BankMgmt.MW.BusinessLayer.SoTietKiemBUS();
+            stkBus.AddSoTietKiem(stk);
+            return Request.CreateResponse(HttpStatusCode.Created, stk);
+        }
+    }
+}
+}
