2d3af091a0afd900fba8b124e9b7a86cc65c5325
Khoa Pham <khoapham10101992@gmail.com>
2017-04-16 22:11:20 +0700

    + Add GiaoDichBUS.cs and RutTienController.cs Chuc nang Rut Tien by Khoa

---
+++ Code/Together_BankManagement_MiddleWare/CSharp.MiddleWare/BankManagement-MiddleWare/BankMgmt.MW.BusinessLayer/GiaoDichBUS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankMgmt.MW.BusinessLayer
+{
+    class GiaoDichBUS
+    {
+        private readonly IGenericDataRepository<GiaoDich> _giaoDichRepo;
+
+        public GiaoDichBUS()
+        {
+            _giaoDichRepo = new GenericDataRepository<GiaoDich>();
+        }
+
+        public List<GiaoDich> GetAllGiaoDich()
+        {
+            return _giaoDichRepo.GetAll().ToList();
+        }
+
+        public void AddGiaoDich(params GiaoDich[] giaoDich)
+        {
+            _giaoDichRepo.Add(giaoDich);
+        }
+    }
+}
