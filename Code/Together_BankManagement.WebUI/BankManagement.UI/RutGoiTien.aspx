b80daf8cb1809a7628f7e6645bd03e86cc1834db
KhoaPham1010 <khoapham10101992@gmail.com>
2017-06-11 00:38:57 +0700

    + Thêm UI Rút tiền và Gởi Tiền

---
+++ Code/Together_BankManagement.WebUI/BankManagement.UI/RutGoiTien.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageOne.Master" AutoEventWireup="true" CodeBehind="RutGoiTien.aspx.cs" Inherits="BankManagement.UI.RutGoiTien" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="assets/js/bootstrap-datepicker-1.6.4-dist/css/bootstrap-datepicker3.min.css" rel="stylesheet" />
+    <script src="assets/js/bootstrap-datepicker-1.6.4-dist/js/bootstrap-datepicker.min.js"></script>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContainer" runat="server">
+
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <div class="filter-left">
+                <div>
+                    <b><asp:Label ID="maKhachHang" runat="server" Enabled="True"></asp:Label></b>
+                </div>
+                <div>
+                   <b> <asp:Label ID="hoTen" runat="server"  Enabled="True"></asp:Label></b>
+                </div>
+                <div>
+                    <b><asp:Label ID="cMND" runat="server"  Enabled="True"></asp:Label></b>
+                </div>
+                <div>
+                  <b> <asp:Label ID="soTien" runat="server"  Enabled="True"></asp:Label></b>
+                </div>
+            </div>
+            <div class="filter-right">
+                <div>
+                    <asp:TextBox ID="txtMaKhachHang" runat="server" CssClass="form-control" Width="316px" Enabled="False" /><asp:TextBox ID="txtMaKhachHangThiet" runat="server" CssClass="hide" Width="316px" />
+                </div>
+                <div>
+                    <asp:TextBox ID="txtHoTenKhachHang" runat="server" CssClass="form-control" Width="316px" Enabled="false" />
+                </div>
+                <div>
+                    <asp:TextBox ID="txtCMND" runat="server" CssClass="form-control" Width="316px" TextMode="Number" />
+                </div>
+                <div>
+                    <asp:TextBox ID="txtSoTien" runat="server" CssClass="form-control" Width="316px" TextMode="Number" />
+                </div>
+                <div class="text-right">
+                    <asp:Button ID="btnRutGoi" runat="server" Text="" CssClass="active" OnClick="btnRutGoi_Click" />
+                </div>
+            </div>
+        </div>
+
+
+        </div>
+    
+
+    <script type="text/javascript">
+
+        $('.input-daterange').datepicker({
+            autoclose: true,
+            endDate: "-1d",
+            format: "dd/mm/yyyy"
+        });
+
+      
+
+        $('[id$=txtCMND]').keypress(function (event) {
+
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if (keycode == '32') {
+                if ($('[id$=txtCMND]').val() == '') {
+                    alert('Chua Nhap Chung Minh Nhan Dan');
+                }
+                else {
+                    var req = $.ajax({
+                        url: 'http://localhost:8080/api/kh/cmnd/' + $('[id$=txtCMND]').val(),
+                        dataType: 'json',
+                        timeout: 30 * 1000
+                    });
+                    alert(req);
+                    req.done(function (data) {
+                        //alert('Lấy Thông tin thành công');
+                        //console.log(data);
+                        $.each(data, function (index, kh) {
+                            $("[id$=txtMaKhachHang]").val(kh.maKhachHang);
+                            $("[id$=txtHoTenKhachHang]").val(kh.hoTen);
+                            $("[id$=txtMaKhachHangThiet]").val(kh.maKhachHang);
+                        });
+
+                    });
+                    req.fail(function (jqXHR, textStatus, error) {
+                        console.log(textStatus);
+                        console.log(error);
+                        console.log(jqXHR);
+                    });
+                }
+            }
+            event.stopPropagation();
+        });
+
+      
+
+    </script>
+
+</asp:Content>
+
---
+++ Code/Together_BankManagement.WebUI/BankManagement.UI/RutGoiTien.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BankManagement.UI.Common;
+using System.Globalization;
+namespace BankManagement.UI
+{
+    public partial class RutGoiTien : System.Web.UI.Page
+    {
+        private int _LoaiGiaoDich
+        {
+            get
+            {
+                if (Request.QueryString.Count > 0)
+                {
+                    int maLoaiGiaoDich;
+                    if (int.TryParse(Request.QueryString["loaiGD"].Trim(), out maLoaiGiaoDich))
+                    {
+                        return maLoaiGiaoDich;
+                    }
+                }
+                return -1;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (_LoaiGiaoDich == 1)
+            {
+                maKhachHang.Text = "Mã Khách Hàng Rút Tiền: ";
+                hoTen.Text = "Khách Hàng Rút Tiền: ";
+                cMND.Text = "Chứng Minh Nhân Dân: ";
+                soTien.Text = "Số Tiền Cần Rút: ";
+                btnRutGoi.Text = "Rút Tiền";
+            }
+            if (_LoaiGiaoDich == 2)
+            {
+                maKhachHang.Text = "Mã Khách Hàng Gởi Tiền: ";
+                hoTen.Text = "Khách Hàng Gởi Tiền: ";
+                cMND.Text = "Chứng Minh Nhân Dân: ";
+                soTien.Text = "Số Tiền Cần Gởi: ";
+                btnRutGoi.Text = "Gởi Tiền";
+            }
+        }
+
+        protected void btnRutGoi_Click(object sender, EventArgs e)
+        {
+            List<khachHang> lskh = GetKH(int.Parse(txtMaKhachHangThiet.Text.Trim().ToString()));
+            khachHang kh = lskh[0];
+            if (_LoaiGiaoDich == 1)
+            {
+                // Kiểm tra còn tiền rút hay ko
+                if (kh.soDuTaiKhoan > double.Parse(txtSoTien.Text.Trim()))
+                {
+                    ServiceConnector.GetDataFromServiceByPost<giaoDich>("api/gd/add", CreateGiaoDich(), false);
+                    kh.soDuTaiKhoan = kh.soDuTaiKhoan - double.Parse(txtSoTien.Text.Trim());
+                    UpdateKH(kh);
+                    ClearData();
+                }
+                else
+                {
+                    Console.WriteLine("So Tien Rut Lon Hon So Du Tai Khoan");
+                }
+            }
+            else
+            {
+                if (_LoaiGiaoDich == 2)
+                {
+                    // Gởi tiền ko cần kiểm tra
+                    ServiceConnector.GetDataFromServiceByPost<giaoDich>("api/gd/add", CreateGiaoDich(), false);
+                    kh.soDuTaiKhoan = kh.soDuTaiKhoan + double.Parse(txtSoTien.Text.Trim());
+                    UpdateKH(kh);
+                    ClearData();
+                }
+            }
+
+        }
+        private void ClearData()
+        {
+            txtMaKhachHang.Text = null;
+            txtHoTenKhachHang.Text = null;
+            txtCMND.Text = null;
+            txtSoTien.Text = null;
+        }
+        private void UpdateKH(khachHang kh)
+        {
+            ServiceConnector.InsertOrUpdate<khachHang>("api/kh/upkh", kh, true);
+        }
+        private giaoDich CreateGiaoDich()
+        {
+            var ct = new giaoDich();
+            ct.maKHGiaoDich = int.Parse(txtMaKhachHangThiet.Text.Trim().ToString());
+            //ct.ngayGiaoDich = DateTime.ParseExact(DateTime.Now.ToString().Trim(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            ct.ngayGiaoDich = DateTime.Now;
+            ct.soTienGiaoDich = double.Parse(txtSoTien.Text.Trim());
+            ct.maGDVien = 1;
+            if (_LoaiGiaoDich==1)
+            {
+                ct.maLoaiGD = 1;
+            }
+            else
+            {
+                ct.maLoaiGD = 2;
+            }
+            return ct;
+        }
+        private List<khachHang> GetKH(int makh)
+        {
+            return ServiceConnector.GetDataFromServiceByGet<khachHang>("api/kh/id/" + makh, true);
+        }
+
+
+    }
+    public class giaoDich
+    {
+        public int maGiaoDich { get; set; }
+        public Double soTienGiaoDich { get; set; }
+        public DateTime ngayGiaoDich { get; set; }
+        public int maKHGiaoDich { get; set; }
+        public int maGDVien { get; set; }
+        public int maLoaiGD { get; set; }
+    }
+}
